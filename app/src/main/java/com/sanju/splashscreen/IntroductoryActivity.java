40fef1bab5bdb359a51267defba214fd16d7bf96
LENOVO <mailmeinmymail0@gmail.com>
2020-11-05 23:17:37 +0530

    on boarding liquid swipping

---
+++ app/src/main/java/com/sanju/splashscreen/IntroductoryActivity.java
@@ -0,0 +1,71 @@
+package com.sanju.splashscreen;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.airbnb.lottie.LottieAnimationView;
+
+public class IntroductoryActivity extends AppCompatActivity {
+
+    ImageView logo,appName,splashImg;
+    LottieAnimationView lottieAnimationView;
+    private static final int NUM_PAGES = 3;
+    private ViewPager viewPager;
+    private ScreenSlidePagerAdapter pagerAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_introductory);
+
+        logo = findViewById(R.id.logo);
+        appName = findViewById(R.id.app_name);
+        splashImg = findViewById(R.id.img);
+        lottieAnimationView = findViewById(R.id.lottie);
+
+        viewPager = findViewById(R.id.pager);
+        pagerAdapter = new ScreenSlidePagerAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(pagerAdapter);
+
+        splashImg.animate().translationY(-2500).setDuration(1000).setStartDelay(4000);
+        appName.animate().translationY(1400).setDuration(1000).setStartDelay(4000);
+        logo.animate().translationY(2000).setDuration(1000).setStartDelay(4000);
+        lottieAnimationView.animate().translationY(1400).setDuration(1000).setStartDelay(4000);
+    }
+
+    private class ScreenSlidePagerAdapter extends FragmentStatePagerAdapter{
+
+        public ScreenSlidePagerAdapter(@NonNull FragmentManager fm) {
+            super(fm);
+        }
+
+        @NonNull
+        @Override
+        public Fragment getItem(int position) {
+            switch (position){
+                case 0:
+                    OnboardingFragment1 tab1 = new OnboardingFragment1();
+                    return tab1;
+                case 1:
+                    OnboardingFragment2 tab2 = new OnboardingFragment2();
+                    return tab2;
+                case 2:
+                    OnboardingFragment3 tab3 = new OnboardingFragment3();
+                    return tab3;
+            }
+            return null;
+        }
+
+        @Override
+        public int getCount() {
+            return NUM_PAGES;
+        }
+    }
+}
